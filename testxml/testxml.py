22594645df10d567aebc23d4555552d0b08a8025
ashllll <lqyimp@gmail.com>
2022-08-14 17:38:15 +0800

    指纹蒙版参数v2.0

--- testxml/testxml.py
+++ testxml/testxml.py
@@ -1,18 +1,18 @@
-# coding=utf-8
+# coding:utf-8
 # https://codeantenna.com/a/3F24HagWPR 来源
 import os
 import os.path
 import xml.dom.minidom
 
 # path="../xml/"
-filename1 = input("TXT文档：")
-filename2 = input("UDFPXML路径：")
-with open(filename1, "r") as f:  #打开文件
+path = input("指纹蒙版文件路径：")
+txt = input("生成的XTX文件：")
+with open(txt, "r") as f:  #打开文件
     data = f.read()  #读取文件
-    print(data)
+    print(data,'请确认文件是否正确！')
 f.close()
 #'''/home/kanghao/learning_something/about_xml/xml/' #Mac与Win反斜线定义不一样，开发环境为Mac，Win上注意替换为"\"'''
-files = os.listdir(filename2)  # 得到文件夹下所有文件名称
+files = os.listdir(path)  # 得到文件夹下所有文件名称
 s = []
 for xmlFile in files:  # 遍历文件夹
     if not os.path.isdir(xmlFile):  # 判断是否是文件夹，不是文件夹才打开
@@ -21,7 +21,7 @@ for xmlFile in files:  # 遍历文件夹
         # xml读取操作
         # 将获取到的xml文件名送入到dom解析
         # 错误代码：dom=xml.dom.minidom.parse(xmlFile)
-        dom = xml.dom.minidom.parse(os.path.join(filename2, xmlFile))
+        dom = xml.dom.minidom.parse(os.path.join(path, xmlFile))
         root = dom.documentElement
 
         ###获取标签对brightness之间的值
@@ -35,12 +35,16 @@ for xmlFile in files:  # 遍历文件夹
             print(type(a))
             brightness[i].firstChild.data = data   # 替换的指纹蒙版数据，还未定义数据来源！！！
             print(brightness[i].firstChild.data)
+
         # 保存修改到xml文件中
-        with open(os.path.join(filename2, xmlFile), 'w') as fh:
+        with open(os.path.join(path, xmlFile), 'w') as fh:
             dom.writexml(fh)
             print('恭喜，写入指纹蒙版数据成功！')
 
-#if __name__ == "__main__":
-    # 要求改的指纹蒙版文件
-    #path = input("请拖入UDFP:")
-    #txt = input("生成的XTX文件：")
+if __name__ == '__main__':
+    path = input("指纹蒙版文件路径：")
+    #打开文件
+    txt = input("生成的XTX文件：")
+
+
+
